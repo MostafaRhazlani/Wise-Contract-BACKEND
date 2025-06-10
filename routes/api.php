3ac504f6cdcd119bbf4981315e7748a374631dec
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-06-10 16:04:32 +0100

    Add user registration functionality to AuthController and update API routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Public routes
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 // Protected routes
