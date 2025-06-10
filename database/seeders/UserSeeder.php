57479a8e1ee2e0c9db8cf500853995d5569a6941
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-06-10 15:47:53 +0100

    Add UserSeeder to create fake user, update DatabaseSeeder to include UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * The current password being used by the seeder.
+     */
+    protected static ?string $password;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create admin user
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => static::$password ??= Hash::make('password123'),
+            'phone' => '1234567890',
+            'role_id' => Role::where('role_name', 'Admin')->first()->id,
+        ]);
+
+        // Create 10 random users
+        User::factory(10)->create();
+    }
+}
