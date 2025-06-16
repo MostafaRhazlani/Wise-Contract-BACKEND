0ae61a4f2a9a0f77396320bba1c3c0160d665a92
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-06-16 10:27:47 +0100

    Add DepartmentController with index methods and API route for listing departments

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DepartmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,4 +13,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'checkAuthenticatedUser']);
+
+    // department route
+    Route::get('/department', [DepartmentController::class, 'index']);
 });
